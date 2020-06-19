800df59d47f4840c5220aa0954feafb3322fbc3b
Jason Li <iverefiner@gmail.com>
2020-06-19 18:47:22 -0500

    added .env for apikey

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import { VictoryPie } from 'victory';
+
+
 
 function App() {
+  const [schoolData, setSchoolData] = useState(null)
+
+  useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }[])
   return (
     <div className="App">
       <header className="App-header">
