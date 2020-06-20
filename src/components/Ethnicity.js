50a6c55d4beadc4f4b398a90c6dbdb1de2eaa35b
Jason Li <iverefiner@gmail.com>
2020-06-20 16:20:04 -0500

    converted data over to match graph requirements. graphs working but need to format graph and strings

--- src/components/Ethnicity.js
+++ src/components/Ethnicity.js
@@ -2,7 +2,18 @@ import React from 'react'
 import { VictoryPie } from 'victory';
 
 function Ethnicity({ethnicity}) {
-  return <div>{console.log(ethnicity)}</div>;
+const convertedData = [];
+
+for (let [key, value] of Object.entries(ethnicity)) {
+  convertedData.push({ x: `${key}`, y: `${value}` });
+}
+
+console.log(convertedData);
+return (
+  <div>
+    <VictoryPie data={convertedData} />
+  </div>
+);
 }
 
 export default Ethnicity
--- src/components/ProgramPercentages.js
+++ src/components/ProgramPercentages.js
@@ -2,11 +2,20 @@ import React from 'react'
 import { VictoryPie } from 'victory';
 
 function ProgramPercentages({programPercentages}) {
+const convertedData = [];
+
+for (let [key, value] of Object.entries(programPercentages)) {
+  convertedData.push({x:`${key}`, y:`${value}`});
+}
+
+console.log(convertedData)
   return (
     <div>
-    {console.log(programPercentages)}
+      <VictoryPie
+        data={convertedData}
+      />
     </div>
-  )
+  );
 }
 
 export default ProgramPercentages
