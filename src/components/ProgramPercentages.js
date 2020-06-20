cfe88cf5c795215afbff6ed020c660f68ac2397c
Jason Li <iverefiner@gmail.com>
2020-06-19 21:51:27 -0500

    added new School, ProgramPercentages, Ethicity components and an index

---
+++ src/components/ProgramPercentages.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ProgramPercentages() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProgramPercentages
+
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as School } from './School';
+export { default as ProgramPercentages } from './ProgramPercentages';
+export { default as Ethnicity } from './Ethnicity';
